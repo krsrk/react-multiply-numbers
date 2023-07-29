be1ab7d0432152ca9d7fd2086e6375990f5b1373
Chris Becerra <christian.becerra@qualtopequiposextendidos.com>
2023-07-29 00:04:53 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
+function MultiplyNumberForm() {
+  const [numberOne, setNumberOne] = useState(0)
+  const [numberTwo, setNumberTwo] = useState(0)
+  const [multiplyResult, setMultiplyResult] = useState(0)
+  const multiplyNumbers = () => {
+    let result = 0
+
+    for (let i=0; i<numberTwo; i++) {
+      result += parseInt(numberOne)
+    }
+
+    setMultiplyResult(result)
+  }
+
+  return (
+      <div>
+          <form>
+            <label>Number one:
+              <input
+                  type="text"
+                  value={numberOne}
+                  onChange={(e) => setNumberOne(e.target.value)}
+              />
+            </label><br />
+            <label>Number two:
+              <input
+                  type="text"
+                  value={numberTwo}
+                  onChange={(e) => setNumberTwo(e.target.value)}
+              />
+            </label>
+            <br />
+          </form>
+          <button
+              onClick={ multiplyNumbers }
+          >
+              Multiply !
+          </button>
+          <br />
+          <label>The result is: {multiplyResult}</label>
+      </div>
+  )
+}
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MultiplyNumberForm />
       </header>
     </div>
   );
